3f938b26d936ed4f5b2b28c8a543ae840f3d157e
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2019-06-04 10:47:19 +0200

    Added library and tests

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# safer-get
+# Safer get
+
 Small utility for accessing deep nested parameters in Typescript
+
+## Usage
+
+- `safer(object, key | keyArray[], defaultValue, excludeNull = false])` - Gets the value at path of object. If the resolved value is `undefined`, the defaultValue is returned in its place.
+
+keyArray is _typed_ and has a maximum depth of 7.
+
+#### Options
+
+| Option         | Description                                                                      | Default Value |
+| -------------- | -------------------------------------------------------------------------------- | ------------- |
+| `defaultValue` | If the resolved value is `undefined`, the defaultValue is returned in its place. | `undefined`   |
+
+```js
+import { safer } from 'safer-get'
+
+const data = { its: { really: { super: { nested: undefined } } } }
+const result = safer(data, ['its', 'really', 'super', 'nested'], 'defaultValue')
+```
---
+++ __tests__/safer-get.spec.ts
@@ -0,0 +1,35 @@
+import { safer } from '../src/index'
+
+describe('safer get', function() {
+  let obj = {
+    a: {
+      b: { c: { d: { g: undefined as undefined | string, f: null as null } } }
+    },
+    b: {}
+  }
+
+  it('should return the value when exists (with string notation)', function() {
+    const result = safer(obj, 'a')
+    expect(result).toEqual(obj.a)
+  })
+
+  it('should return the value when exists (with array notation)', function() {
+    const result = safer(obj, ['a', 'b'])
+    expect(result).toEqual(obj.a.b)
+  })
+
+  it('should return the default value when not exists', function() {
+    const result = safer(obj, ['a', 'b', 'c', 'd', 'g'], 'default')
+    expect(result).toEqual('default')
+  })
+
+  it('should return undefined otherwise', function() {
+    const result = safer(obj as any, ['b', 'hello'])
+    expect(result).toBeUndefined()
+  })
+
+  it('should return undefined even when the value is null', function() {
+    const result = safer(obj, ['a', 'b', 'c', 'd', 'f'])
+    expect(result).toBeUndefined()
+  })
+})
